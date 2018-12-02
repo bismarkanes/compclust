41137f4776a0c4cd37270e582d969c7527465e1a
BISMARK AGUSTINE JOHANNES <bismark.john.anes@gmail.com>
2018-12-02 22:48:36 +0700

    Updated connection to db

--- node/index.js
+++ node/index.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT;
+const pg = require('pg');
+const client = new pg.Client({
+  connectionString: `postgres://postgres:postgres@${process.env.DB_HOST}:${process.env.DB_PORT}/postgres`,
+});
 
 app.get('/', (req, res) => {
   res.json({
     hostname: process.env.HOSTNAME,
     port: process.env.PORT,
+    db: `${process.env.DB_HOST}:${process.env.DB_PORT}`,
+    redis: `${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
   });
 });
 
 app.get('/ping', (req, res) => {
   res.send('PONG');
+});
+
+app.get('/tests/db', (req, res) => {
+  client.connect((err) => {
+    if (err) {
+      return res.status(400).send('ERROR DB Connection!');
+    }
+
+    res.send('DB connection success!');
+    client.end();
+  });
 });
 
 app.listen(port, (err) => {
